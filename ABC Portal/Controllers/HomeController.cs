20092fb81dbdca690c687eaeb6525b0d05560bc7
brad gill <raddack@gmail.com>
2018-07-29 21:45:48 -0600

    Init Commit

---
+++ ABC Portal/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ABC_Portal.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.ABCUser LoggedInUser = new Models.ABCUser();
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.ABCUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(Request.Form["UserName"], Request.Form["Password"]))
+                {
+                   
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                    LoggedInUser = user;
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                   
+                    ModelState.AddModelError("", "Login data is incorrect!");
+                }
+            }
+            return View(user);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+    }
+}
---
+++ ABC Portal/Models/ABCUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ABC_Portal.Models
+{
+    public class ABCUser
+    {
+
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")] 
+        public string Password { get; set; } = "";
+
+        [Display(Name = "Remember on this computer")]
+        public bool RememberMe { get; set; } = false;
+
+        /// <summary>
+        /// Checks if user with given password exists in the database
+        /// </summary>
+        /// <param name="_username">User name</param>
+        /// <param name="_password">User password</param>
+        /// <returns>True if user exist and password is correct</returns>
+        public bool IsValid(string _username, string _password)
+        {
+
+            return true;
+
+        }
+
+
+    }
+}
